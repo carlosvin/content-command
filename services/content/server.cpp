c2c3a78767d8a24064c9d80ca004925a63c9fd3d
Carlos Martin Sanchez <carlosvin@gmail.com>
2015-03-22 18:26:45 +0100

    running the server

--- services/content/server.cpp
+++ services/content/server.cpp
@@ -5,25 +5,23 @@
 #include <grpc/grpc.h>
 
 #include "ContentCmdHandlerImpl.h"
-
-//#include <grpc++/server.h>
-//#include <grpc++/server_builder.h>
-//#include <grpc++/server_context.h>
-//#include <grpc++/server_credentials.h>
-//#include <grpc++/status.h>
-//#include "cms.pb.h"
-//
-//using grpc::Server;
-//using grpc::ServerBuilder;
-//using grpc::ServerContext;
-//using grpc::Status;
+#include "ServerRunner.h"
 
 
+using namespace std;
+using namespace cms;
 
 int main(int argc, char** argv) {
+
+	const string address = "0.0.0.0:10001";
+
 	grpc_init();
 
 	cms::ContentCmdHandlerImpl content_cmd_handler{};
+
+	cms::ServerRunner server_runner {address, {&content_cmd_handler}};
+
 	grpc_shutdown();
+
 	return 0;
 }
