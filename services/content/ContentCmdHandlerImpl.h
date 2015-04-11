5ac694b135e5ea19f2b55c45f86bbac4f1285a40
carlosvin <carlosvin@gmail.com>
2015-04-11 20:09:34 +0200

    removing autotools files

---
+++ services/content/ContentCmdHandlerImpl.h
@@ -0,0 +1,33 @@
+/*
+ * ContentCmdHandlerImpl.h
+ *
+ *  Created on: 22/3/2015
+ *      Author: carlos
+ */
+
+#ifndef CONTENT_CONTENTCMDHANDLERIMPL_H_
+#define CONTENT_CONTENTCMDHANDLERIMPL_H_
+
+//#include <grpc++/server_credentials.h>
+//#include <grpc++/server_builder.h>
+
+#include <grpc++/server.h>
+#include <grpc++/server_context.h>
+#include <grpc++/status.h>
+#include "cms.pb.h"
+
+namespace cms {
+
+
+class ContentCmdHandlerImpl final: public ContentCmdHandler::Service
+{
+
+public:
+	explicit ContentCmdHandlerImpl();
+	~ContentCmdHandlerImpl();
+	grpc::Status create(grpc::ServerContext* context, const Content* request, CmdResponse* response) override;
+
+};
+
+}
+#endif /* CONTENT_CONTENTCMDHANDLERIMPL_H_ */
