cf8a691bff1f9e558ed30623f1898d8a10aa488d
Carlos Martin Sanchez <carlosvin@gmail.com>
2015-05-03 21:13:08 +0200

    removed wall

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,11 @@ MAINTAINER Carlos Martin Sanchez "carlosvin@gmail.com"
 RUN apt-get update
 RUN apt-get install -y scons
 
+
 #Install cms light
+
 RUN git clone https://github.com/carlosvin/cms-light.git
 WORKDIR /cms-light
+RUN git pull
 RUN scons
 ENTRYPOINT ./content_server
--- SConstruct.py
+++ SConstruct.py
@@ -2,7 +2,7 @@ env = Environment(
 	CPPPATH=['src/content_server/headers'],
 	CPPDEFINES=[],
 	LIBS=['grpc++_unsecure', 'grpc', 'gpr', 'protobuf', 'pthread', 'dl'],
-    CXXFLAGS=['-std=c++11', '-g', '-Wall'],
+    CXXFLAGS=['-std=c++11', '-g'],
 	#LIBPATH=["/usr/lib", "/usr/local/lib", '/usr/lib64'],
     #LINKFLAGS="--static"
 )
