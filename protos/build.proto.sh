3fdf8f055fdb8bb924704a08aee30cabf137471c
carlos <carlosvin@gmail.com>
2015-05-15 15:49:57 +0200

    creating new api

--- protos/build.proto.sh
+++ protos/build.proto.sh
@@ -8,5 +8,3 @@ OUT_DIR=.
 protoc -I$PROTOS_PATH --grpc_out=$OUT_DIR --plugin=protoc-gen-grpc=$GRPC_CPP_PLUGIN_PATH $1
 protoc -I$PROTOS_PATH --cpp_out=$OUT_DIR $1
 
-mv -f *.h $2/headers
-mv -f *.cc $2/cpp
