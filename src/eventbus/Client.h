06d9213b5e5db23e13b7a020d3c1b82a0b39a1d2
Carlos Martin Sanchez <carlosvin@gmail.com>
2015-05-21 21:24:46 +0200

    eventbus client

---
+++ src/eventbus/Client.h
@@ -0,0 +1,15 @@
+/*
+ * Client.h
+ *
+ *  Created on: 21 de may. de 2015
+ *      Author: carlos
+ */
+
+#ifndef EVENTBUS_CLIENT_H_
+#define EVENTBUS_CLIENT_H_
+
+
+
+
+
+#endif /* EVENTBUS_CLIENT_H_ */
